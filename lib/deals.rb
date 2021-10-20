ffa91eb7f67e30bc7be1a4cc6ee6dacec17d0ce2
Hannah Malm <hannahemalm616@gmail.com>
2021-10-19 21:18:27 -0400

    first commit

---
+++ lib/deals.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "deals/version"
+
+module Deals
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/deals/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Deals
+  VERSION = "0.1.0"
+end
